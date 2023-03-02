5571ac40d4231254fbd8be190a451b2915e764eb
Beloved Adejuyigbe <64827432+beladactions@users.noreply.github.com>
2023-03-02 13:36:29 +0100

    Via upload

---
+++ Javascriptvar.js
@@ -0,0 +1,24 @@
+/*JavaScript variables are used to store data values in memory which can hold different types of values, 
+including numbers, strings, booleans, objects, arrays, and functions. Variables in JavaScript are declared 
+using the var, let, or const keywords. 
+var was the original keyword for declaring variables in JavaScript, 
+but let and const were introduced in newer versions of the language to provide additional flexibility and 
+clarity in code.
+let is used to declare variables that can be reassigned to new values, 
+while const is used to declare variables whose value cannot be changed after initialization.
+*/
+
+
+// Declare Variables using the 'var' 'let' and 'const' keyword
+
+var myNumber = 10;
+let myString = "Hello, world!";
+const myBoolean = true;
+let myUndefinedVariable;
+
+// Print Out Each of the Variables Declared up there
+
+console.log(myNumber)
+console.log(myString)
+console.log(myBoolean)
+console.log(myUndefinedVariable)
